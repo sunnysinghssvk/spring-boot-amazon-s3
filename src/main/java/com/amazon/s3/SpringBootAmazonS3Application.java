30a013d4d87123418743f2ad4520f7c7d558888e
Sunny <sunnysinghssvk@gmail.com>
2024-03-12 09:41:44 +0530

    Initial Commit

---
+++ src/main/java/com/amazon/s3/SpringBootAmazonS3Application.java
@@ -0,0 +1,13 @@
+package com.amazon.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAmazonS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAmazonS3Application.class, args);
+	}
+
+}
